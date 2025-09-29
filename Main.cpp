3bcdd61cbeb7c260c8117c1990cb583ad8a2a9f6
Founder-TGL <ginsbergaiden@gmail.com>
2025-09-29 23:48:53 +0000

    hello

---
+++ Main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+
+    int apples; // initilization
+    apples = 4; //declarization
+    int bannanas = 5; // initilization + declarization
+
+
+    int fruit = apples + bannanas;
+
+    std::cout << "fruit in basket = " << fruit << "\n";
+
+    return 1;
+}
