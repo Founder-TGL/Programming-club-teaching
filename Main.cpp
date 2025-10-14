8d70f219a9d3eb30d8e88393d1c7ad5bf41a6ef6
Founder-TGL <ginsbergaiden@gmail.com>
2025-10-14 00:03:09 +0000

    bebop

--- Main.cpp
+++ Main.cpp
@@ -1,20 +1,26 @@
 #include <iostream>
 
-int main(){
+void  calculator(int varA, int varB);
 
+int main(){
     int var1;
     int var2;
     int answer;
 
-    std::cout << "what is the first number: ";
-    std::cin >> var1;
-    std::cout << "what is the second number: ";
-    std::cin >> var2;
-    
-    answer = var1 + var2;
-
-    std::cout << var1 << " + " << var2 << " = " << answer << "\n";
-
+    while(true){
+        calculator(var1, var2);
+    }
 
     return 1;
+}
+
+void  calculator(int varA, int varB){
+    std::cout << "what is the first number: ";
+    std::cin >> varA;
+    std::cout << "what is the second number: ";
+    std::cin >> varB;
+
+    int answer = varA + varB;
+    
+    std::cout << varA << " + " << varB << " = " << answer << "\n";
 }
